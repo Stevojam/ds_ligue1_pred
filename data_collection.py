3a1b9041e7158ca552bb26c8765cbb55755d4e5e
Steve James <91149539+Stevojam@users.noreply.github.com>
2023-11-12 22:08:09 +0100

    Add files via upload

--- data_collection.py
+++ data_collection.py
@@ -1,12 +1,13 @@
 import TransferMarkt_scraper as tms
 import pandas as pd
 
-df_squadValues = tms.get_squadValues(2011, 2022)
-df_transferDetails = tms.get_transferDetails(2011, 2022)
+df_squadValues = tms.get_squadValues(2011, 2023)
+df_transferDetails = tms.get_transferDetails(2011, 2023)
 df_standings = tms.get_seasonTable(2011, 2022)
 
 df_squadValues.to_csv('squad_values.csv', index = False)
 df_transferDetails.to_csv('transfer_details.csv', index = False)
 df_standings.to_csv('standings.csv', index = False)
+
 
 
